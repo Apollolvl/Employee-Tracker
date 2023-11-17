2f9ee0a0014651b23bd7fcbc532c5110d504ea72
Apollo Vavra <apollolvavra@gmail.com>
2023-11-17 13:57:39 -0600

    started adding functions for prompts

--- node.js
+++ node.js
@@ -2,42 +2,181 @@ require('dotenv').config();
 const mysql = require('mysql');
 const util = require('util');
 
-// Access environment variables
-const host = process.env.DB_HOST;
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const database = process.env.DB_NAME;
-const port = process.env.DB_PORT;
-
-// Create a MySQL connection
 const connection = mysql.createConnection({
-    host,
-    user,
-    password,
-    database,
-    port
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
-const query = util.promisify(connection.query).bind(connection);
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+    startApp();
+});
 
-async function main() {
-    try {
-        // Connect to JawsDB database
-        await query('SELECT 1');
-
-        // Insert a new department
-        await query("INSERT INTO Departments (DepartmentName) VALUES ('IT')");
-
-        // Retrieve departments
-        const departments = await query('SELECT * FROM Departments');
-        console.log('Departments:', departments);
-
-    } catch (error) {
-        console.error('Error:', error);
-    } finally {
-        // Close the connection
-        connection.end();
-    }
+function startApp() {
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
+        })
+        .then(answer => {
+            switch (answer.action) {
+                case 'View all departments':
+                    viewAllDepartments();
+                    break;
+                case 'View all roles':
+                    viewAllRoles();
+                    break;
+                case 'View all employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployeeRole();
+                    break;
+                case 'Exit':
+                    connection.end();
+                    console.log('Goodbye!');
+                    break;
+            }
+        });
 }
 
-main();
+function viewAllDepartments() {
+    connection.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        startApp();
+    });
+}
+
+function viewAllRoles() {
+    connection.query('SELECT * FROM role', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        startApp();
+    });
+}
+
+function viewAllEmployees() {
+    connection.query('SELECT * FROM employee', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        startApp();
+    });
+}
+
+function addDepartment() {
+    inquirer
+        .prompt({
+            type: 'input',
+            name: 'departmentName',
+            message: 'Enter the name of the department:'
+        })
+        .then(answer => {
+            connection.query(
+                'INSERT INTO department (name) VALUES (?)',
+                [answer.departmentName],
+                (err, res) => {
+                    if (err) throw err;
+                    console.log('Department added!');
+                    startApp();
+                }
+            );
+        });
+}
+
+function addRole() {
+    inquirer
+        .prompt({
+            type: 'input',
+            name: '',
+            message: 'Enter the name of the department:'
+        })
+        prompt({
+            type: 'input',
+            name: 'departmentName',
+            message: 'Enter the name of the department:'
+        })
+        prompt({
+            type: 'input',
+            name: 'departmentName',
+            message: 'Enter the name of the department:'
+        })
+        .then(answer => {
+            connection.query(
+                'INSERT INTO department (name) VALUES (?)',
+                [answer.departmentName],
+                (err, res) => {
+                    if (err) throw err;
+                    console.log('Department added!');
+                    startApp();
+                }
+            );
+        });
+
+}
+
+function addEmployee() {
+    inquirer
+        .prompt({
+            type: 'input',
+            name: 'departmentName',
+            message: 'Enter the name of the department:'
+        })
+        .then(answer => {
+            connection.query(
+                'INSERT INTO department (name) VALUES (?)',
+                [answer.departmentName],
+                (err, res) => {
+                    if (err) throw err;
+                    console.log('Department added!');
+                    startApp();
+                }
+            );
+        });
+
+}
+
+function updateEmployeeRole() {
+    inquirer
+        .prompt({
+            type: 'input',
+            name: 'departmentName',
+            message: 'Enter the name of the department:'
+        })
+        .then(answer => {
+            connection.query(
+                'INSERT INTO department (name) VALUES (?)',
+                [answer.departmentName],
+                (err, res) => {
+                    if (err) throw err;
+                    console.log('Department added!');
+                    startApp();
+                }
+            );
+        });
+
+}
